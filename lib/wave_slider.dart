f1f9db06ed8e7f9d6bb7d7c0f126c6e92cdad910
Gulnara Kurmanova <gulnarakurmanova@Gulnaras-MacBook-Air-2.local>
2021-10-22 11:43:05 +0600

    controllers added

--- lib/wave_slider.dart
+++ lib/wave_slider.dart
@@ -78,4 +78,68 @@ class _WaveSliderState extends State<WaveSlider> {
   }
 }
 
+class WaveSliderController  extends ChangeNotifier{
+  final AnimationController controller;
+  SliderState _state = SliderState.resting;
+
+  WaveSliderController({required TickerProvider vsync}): controller = AnimationController(vsync: vsync) {
+    controller
+      ..addListener(_onProgressUpdate)
+      ..addStatusListener(_onStatusUpdate);
+      }
+
+  double get progress => controller.value;
+
+  SliderState get state => _state;
+
+  void _onProgressUpdate(){
+    notifyListeners();
+  }
+
+  void _onStatusUpdate(AnimationStatus status){
+    if (status == AnimationStatus.completed){
+      _onTransitionCompleted();
+    }
+  }
+
+  void _onTransitionCompleted(){
+    if (_state == SliderState.stopping) {
+      setStateToResting();
+    }
+  }
+
+  void _startAnimation(){
+    controller.duration = Duration(milliseconds: 500);
+    controller.forward(from: 0.0);
+    notifyListeners();
+  }
+
+  void setStateToResting(){
+    _state = SliderState.resting;
+  }
+
+  void setStateToStart(){
+    _state = SliderState.starting;
+  }
+
+  void setStateToSliding(){
+    _state = SliderState.sliding;
+  }
+
+  void setStateToStopping(){
+    _startAnimation();
+    _state = SliderState.stopping;
+  }
+
+
+
+}
+
+enum SliderState {
+  starting,
+  resting,
+  sliding,
+  stopping,
+}
+
 
